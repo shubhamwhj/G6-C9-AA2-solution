2dd50ddcdc35ca064111cc1791484b27610279ac
shubhamwhj <shubham.verma@whitehatjr.com>
2021-09-20 19:55:37 +0530

    SA solution

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ function setup()
  createCanvas(400,400)
  box = createSprite(40,40,40,40);
  box.shapeColor = color(255);
- box.x=  ;
+ box.x=0;
  box.y=150;
-
+ box.velocityX=5
 }
 
 function draw()
