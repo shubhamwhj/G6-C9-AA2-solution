8330900e58acb8943412223722d4ecce776a94c9
shubhamwhj <shubham.verma@whitehatjr.com>
2021-09-20 19:47:26 +0530

    TA

--- sketch.js
+++ sketch.js
@@ -2,11 +2,15 @@ var box;
 function setup()
 {
  createCanvas(400,400)
-
+ box = createSprite(40,40,40,40);
+ box.shapeColor = color(255);
+ box.x=100;
+ box.y=150;
+ box.velocityX=5
 }
- 
+
 function draw()
 {
  background(220);
- 
+ drawSprites();
 }
