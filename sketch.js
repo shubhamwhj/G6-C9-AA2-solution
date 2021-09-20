dde3e2454802b15df6a2398ca7d38d60213d6ea1
shubhamwhj <shubham.verma@whitehatjr.com>
2021-09-20 19:53:02 +0530

    SA template

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ function setup()
  createCanvas(400,400)
  box = createSprite(40,40,40,40);
  box.shapeColor = color(255);
- box.x=100;
+ box.x=  ;
  box.y=150;
- box.velocityX=5
+
 }
 
 function draw()
@@ -14,3 +14,4 @@ function draw()
  background(220);
  drawSprites();
 }
+
