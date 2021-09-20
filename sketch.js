22b4ea1685006e69d0914b14f8e88ffdb242bb13
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-09-20 20:10:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ function setup()
  createCanvas(400,400)
  box = createSprite(40,40,40,40);
  box.shapeColor = color(255);
- box.x=0;
+ box.x=30;
  box.y=150;
- box.velocityY=5
+ box.velocityY=-5
  box.rotation=50
 }
 
